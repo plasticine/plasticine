c066ff43ed621353f3c70e296b35867692cfa1ea
Justin Morris <justin@pixelbloom.com>
2009-04-03 19:26:15 +1100

    Modified test.py

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ string = u'ticket#123'
 string = u'ticket#123[hold]'
 string = u'ticket#123[assign:justin]'
 
-regex = r'ticket#(?P<ticket_id>[\d]+)(?:\[(?P<ticket_action>[\w]+)(?::(?P<ticket_assign_to>[\w]+)\])?)?'
+# ////////////////////////////////////////////////////
 
+regex = r'ticket#(?P<ticket_id>[\d]+)(?:\[(?P<ticket_action>[\w]+)(?::(?P<ticket_assign_to>[\w]+)\])?)?'
 regex = re.compile(regex)
 matches = regex.finditer(string)
 spans = []
-
 regex.findall(string)
 
 for match in matches:
@@ -22,5 +22,4 @@ if len(spans) != 0:
 		span = spans.pop()
 		ticket = string[span[0]:span[1]]
 		ticket_id = regex.findall(ticket)
-		
 		print "%s" % (ticket_id)
